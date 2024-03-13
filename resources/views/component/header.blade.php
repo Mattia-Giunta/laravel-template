07ca759f14a0a0dd7cfa05f533d764059c46830f
Mattia-Giunta <mattia.giunta.90@hotmail.com>
2024-03-13 15:12:55 +0100

    creato template

---
+++ resources/views/component/header.blade.php
@@ -0,0 +1,14 @@
+
+<div class="container">
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/about">About</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+    </ul>
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.layout')
+
+@section('title', 'benvenuti | Home')
+
+@section('content')
+
+    <h1>Hello World</h1>
+
+    <h1>Sono la pagina home</h1>
+
+    <h3>My name is {{$name}}</h3>
+    
+@endsection
